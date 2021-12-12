309b4215c6661999bc3181ba326cf349dda65442
Douglas Porfiro <douglasporfiro25@gmail.com>
2021-12-12 18:49:44 -0300

    DTO, SQL and find by ID

--- backend/src/main/java/com/daporfiro/testcrud/resources/ClientResource.java
+++ backend/src/main/java/com/daporfiro/testcrud/resources/ClientResource.java
@@ -5,10 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.daporfiro.testcrud.entities.Client;
+import com.daporfiro.testcrud.dto.ClientDTO;
 import com.daporfiro.testcrud.services.ClientService;
 
 @RestController
@@ -18,10 +19,21 @@ public class ClientResource {
 	@Autowired
 	private ClientService service;
 	
+	
+	// Metodo para buscar todos os clientes
+	
 	@GetMapping
-	public ResponseEntity<List<Client>> findall(){
-		List<Client> list = service.findall();
+	public ResponseEntity<List<ClientDTO>> findall(){
+		List<ClientDTO> list = service.findall();
 		return ResponseEntity.ok().body(list);
+	}
+	
+	// Metodo de busca por ID
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<ClientDTO> findById(@PathVariable Long id){
+		ClientDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
 	}
 
 }
--- backend/src/main/java/com/daporfiro/testcrud/services/ClientService.java
+++ backend/src/main/java/com/daporfiro/testcrud/services/ClientService.java
@@ -1,10 +1,14 @@
 package com.daporfiro.testcrud.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.daporfiro.testcrud.dto.ClientDTO;
 import com.daporfiro.testcrud.entities.Client;
 import com.daporfiro.testcrud.repositories.ClientRepository;
 
@@ -14,9 +18,21 @@ public class ClientService {
 	@Autowired
 	private ClientRepository repository;
 	
-	public List<Client> findall() {
-		return repository.findAll();
-		
+	@Transactional(readOnly = true)
+	public List<ClientDTO> findall() {
+		List<Client> list = repository.findAll();
+		List<ClientDTO> listDto = new ArrayList<>();
+		for (Client client: list) {
+			listDto.add(new ClientDTO(client));
+		}
+		return listDto;		
+	}
+
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.get();
+		return new ClientDTO(entity);
 	}
 	
 }
